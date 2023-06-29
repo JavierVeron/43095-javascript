239e910f3bd152259bdbc16dd7868de78c2738da
Javier Veron <javier.veron@gmail.com>
2023-06-28 23:47:55 -0300

    Clase 17

---
+++ clase17/index.js
@@ -0,0 +1,10 @@
+/* let mensaje = "Hola Coders!";
+console.log(mensaje); */
+
+/* const productos = [
+    {id:1, nombre:"Coca Cola", precio:700},
+    {id:2, nombre:"Pepsi", precio:650},
+    {id:3, nombre:"Manaos", precio:350}
+];
+console.log(productos); */
+
